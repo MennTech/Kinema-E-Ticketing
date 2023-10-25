7f50fbf6706382ef63d76e9b7de6c9bddb8ec49a
Gabriella Denada <132036675+dendenaa92@users.noreply.github.com>
2023-10-25 10:19:54 +0700

    Update login.blade.php

--- resources/views/user -api/login.blade.php
+++ resources/views/user -api/login.blade.php
@@ -8,51 +8,44 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
+    <style>
+        .navbar-nav {
+            display: flex;
+            align-items: center;
+            margin-right: 10px; /* Sesuaikan jarak antara Login dan Sign Up */
+        }
+        .nav-item {
+            margin-right: 1px; /* Sesuaikan jarak antara Login dan Sign Up */
+        }
+    </style>
+
 </head>
 
 <body style="background-color: #EFEFEF;">
     <!-- navbar -->
-    <nav class="navbar bg-body-tertiary" style="background-color: #FFFFFF;">
-        <nav class="navbar bg-body-tertiary fixed-top">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                    <a class="navbar-brand col" href="#">
-                        <img src="/images/kinema_logo.png" width="105" height="40">
-                    </a>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Masukkan Pencarian Anda">
-                            <button type="button" class="btn btn-primary" style="font-weight: bold;">Cari</button>
-                        </form>
-
-                    <!-- di dalemnya swipenya -->
-                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header">
-                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="login.blade.php">Login</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="signup.blade.php">Sign Up</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="home.blade.php">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#">Tiket Anda</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#">Pesan</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #FFFFFF;">
+        <div class="container-fluid">
+            <a class="navbar-brand col" href="#">
+                <img src="/images/kinema_logo.png" width="105" height="40">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item text-left">
+                        <a class="nav-link active" aria-current="page" href="{{ url('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item text-left">
+                        <a class="nav-link active" aria-current="page" href="{{ url('signup') }}">Sign Up</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Masukkan Pencarian Anda">
+                    <button type="button" class="btn btn-primary" style="font-weight: bold;">Cari</button>
+                </form>
             </div>
-        </nav>
+        </div>
     </nav>
 
     <!-- card login -->
